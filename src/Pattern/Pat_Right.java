934f53068f7e911f477a18c5876769c87945ee32
Preethi-Vaishali <149764470+Preethi-Vaishali@users.noreply.github.com>
2024-04-03 11:35:21 +0530

    Update Pat_Right.java

--- src/Pattern/Pat_Right.java
+++ src/Pattern/Pat_Right.java
@@ -9,7 +9,7 @@ public class Pat_Right {
 		Scanner s = new Scanner(System.in);
 		System.out.print("Enter the number: ");
 		n= s.nextInt();
-		for(int i=1; i<=n; i++)
+		for(int i=1; i<n; i++)
 		{
 			for(int j=1; j<=i; j++) {
 				System.out.print("*");
